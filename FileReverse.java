f50c344c24ea8f3e9957b41452815faea6542a6f
tclark107 <tcjc107@gmail.com>
2020-03-12 15:44:49 -0700

    set up complete

---
+++ FileReverse.java
@@ -0,0 +1,13 @@
+//------------------------------------------------------------------------------------------------------------------------------
+// FileReverse.java
+// This is an excercise in fileio and tokenization.
+// It will also use recursion.
+//
+// Tristan Clark
+//------------------------------------------------------------------------------------------------------------------------------
+
+class FileReverse{
+    public static void main(String args[]) {
+        System.out.println("hello");
+    }
+}
--- README.txt
+++ README.txt
@@ -1,1 +1,2 @@
-This is the start of a new project.
+This is an excercise in fileio and tokenization.
+It will also use recursion.
